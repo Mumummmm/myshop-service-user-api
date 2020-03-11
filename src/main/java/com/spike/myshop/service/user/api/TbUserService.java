797721165a3947b826c678b65c1209c45d17d183
pansy <526915701@qq.com>
2020-03-11 11:36:49 +0800

    init commit

--- src/main/java/com/spike/myshop/service/user/api/TbUserService.java
+++ src/main/java/com/spike/myshop/service/user/api/TbUserService.java
@@ -1,9 +1,11 @@
 package com.spike.myshop.service.user.api;
 
+import com.github.pagehelper.PageInfo;
 import com.spike.myshop.commons.domain.TbUser;
 
 import java.util.List;
 
 public interface TbUserService {
     List<TbUser> selectAll();
+    PageInfo<TbUser> page(int start, int length, TbUser tbUser);
 }
